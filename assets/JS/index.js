4498fa1fd4561e2db43d7ff720fb5adeee0d0983
Manuel Loera <mloera7290@sdsu.edu>
2020-01-30 18:37:17 -0800

    Initial commit, react not implemented yet

---
+++ assets/JS/index.js
@@ -0,0 +1,11 @@
+class QuickQuotePerson{
+    constructor(startDate, endDate, isUnderAge, isTowing) {
+        this.startDate = startDate;
+        this.endDate = endDate;
+        this.isUnderAge = isUnderAge;
+        this.isTowing = isTowing;
+    }
+}
+
+const startDate = document.getElementById('#startDate');
+const endDate = document.querySelector
